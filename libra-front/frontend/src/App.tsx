93526c0b61fb4c7f8d3bab04bbd3b36611637633
noam1813 <devgobouman@gmail.com>
2023-07-10 09:13:42 +0900

    フロントDockerの起動を確認

--- libra-front/frontend/src/App.tsx
+++ libra-front/frontend/src/App.tsx
@@ -1,11 +1,25 @@
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import Home from "./pages/Home";
 import CssBaseLine from "@mui/material/CssBaseline";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
 
+const mdTheme = createTheme({
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#EF5350",
+    },
+    success: {
+      main: "#4caf50",
+    },
+  },
+});
+
 function App() {
   return (
+    <ThemeProvider theme={mdTheme}>
       <BrowserRouter>
         <CssBaseLine />
         <Header />
@@ -13,6 +27,7 @@ function App() {
           <Route path="/" element={<Home />} />
         </Routes>
       </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
